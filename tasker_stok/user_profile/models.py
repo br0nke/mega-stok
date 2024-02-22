0cd19b5228f7e8e3867da668927f6be6c636c230
br0nke <woitekas@gmail.com>
2024-02-22 19:11:40 +0000

    user edit + profile photo adding

--- tasker_stok/user_profile/models.py
+++ tasker_stok/user_profile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), verbose_name=_("user"), on_delete=models.CASCADE)
+    picture = models.ImageField(_("picture"), upload_to='media/', blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("profile")
+        verbose_name_plural = _("profiles")
+
+    def __str__(self):
+        return f"{self.user}"
+
+    def get_absolute_url(self):
+        return reverse("user_detail_current", kwargs={"pk": self.pk})
